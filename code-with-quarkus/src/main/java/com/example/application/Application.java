f10136ccb122f0ddb6466a7cddac030624f91cc4
Marco Collovati <mcollovati@gmail.com>
2023-02-13 17:25:08 +0100

    chore: setup project

---
+++ code-with-quarkus/src/main/java/com/example/application/Application.java
@@ -0,0 +1,17 @@
+package com.example.application;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+
+@Theme(value = "my-hilla-app")
+public class Application implements AppShellConfigurator {
+
+}
