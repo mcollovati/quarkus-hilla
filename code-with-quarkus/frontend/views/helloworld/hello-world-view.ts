92f0707e6396e98eef8bcfa6dc4c9b0a71b9aa94
Dario Götze <dario@ok.de>
2023-02-28 11:40:33 +0100

    Add POJO endpoint

--- code-with-quarkus/frontend/views/helloworld/hello-world-view.ts
+++ code-with-quarkus/frontend/views/helloworld/hello-world-view.ts
@@ -1,34 +1,45 @@
 import '@vaadin/button';
 import '@vaadin/notification';
-import { Notification } from '@vaadin/notification';
+import {Notification} from '@vaadin/notification';
 import '@vaadin/text-field';
+import UserPOJO from 'Frontend/generated/com/example/application/endpoints/helloworld/HelloWorldEndpoint/UserPOJO';
 import * as HelloWorldEndpoint from 'Frontend/generated/HelloWorldEndpoint';
-import { html } from 'lit';
-import { customElement } from 'lit/decorators.js';
-import { View } from '../../views/view';
+import {html} from 'lit';
+import {customElement} from 'lit/decorators.js';
+import {View} from '../../views/view';
 
 @customElement('hello-world-view')
 export class HelloWorldView extends View {
-  name = '';
+    name = '';
 
-  connectedCallback() {
-    super.connectedCallback();
-    this.classList.add('flex', 'p-m', 'gap-m', 'items-end');
-  }
+    connectedCallback() {
+        super.connectedCallback();
+        this.classList.add('flex', 'p-m', 'gap-m', 'items-end');
+    }
 
-  render() {
-    return html`
-      <vaadin-text-field label="Your name" @value-changed=${this.nameChanged}></vaadin-text-field>
-      <vaadin-button @click=${this.sayHello}>Say hello</vaadin-button>
-    `;
-  }
+    render() {
+        return html`
+            <vaadin-text-field label="Your name" @value-changed=${this.nameChanged}></vaadin-text-field>
+            <vaadin-button @click=${this.sayHello}>Say hello</vaadin-button>
+            <vaadin-button @click=${this.sayComplexHello}>Say complex hello</vaadin-button>
+        `;
+    }
 
-  nameChanged(e: CustomEvent) {
-    this.name = e.detail.value;
-  }
+    nameChanged(e: CustomEvent) {
+        this.name = e.detail.value;
+    }
 
-  async sayHello() {
-    const serverResponse = await HelloWorldEndpoint.sayHello(this.name);
-    Notification.show(serverResponse);
-  }
+    async sayHello() {
+        const serverResponse = await HelloWorldEndpoint.sayHello(this.name);
+        Notification.show(serverResponse);
+    }
+
+    async sayComplexHello() {
+        const pojo: UserPOJO = {
+            name: this.name,
+            surname: "surname"
+        }
+        const serverResponse = await HelloWorldEndpoint.sayComplexHello(pojo);
+        Notification.show(serverResponse);
+    }
 }
--- code-with-quarkus/src/main/java/com/example/application/endpoints/helloworld/HelloWorldEndpoint.java
+++ code-with-quarkus/src/main/java/com/example/application/endpoints/helloworld/HelloWorldEndpoint.java
@@ -17,4 +17,46 @@ public class HelloWorldEndpoint {
             return "Hello " + name;
         }
     }
+
+    @Nonnull
+    public String sayComplexHello(@Nonnull UserPOJO user) {
+        System.out.println("User: " + user.name() + " " + user.surname());
+        if (user.surname().isEmpty()) {
+            return "Hello stranger";
+        } else {
+            return "Hello " + user.name() + " " + user.surname();
+        }
+    }
+
+    public static class UserPOJO {
+
+        private String name;
+        private String surname;
+
+        public UserPOJO() {
+        }
+
+        public UserPOJO(String name, String surname) {
+            this.name = name;
+            this.surname = surname;
+        }
+
+        public String name() {
+            return name;
+        }
+
+        public String surname() {
+            return surname;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setSurname(String surname) {
+            this.surname = surname;
+        }
+    }
+
+
 }
