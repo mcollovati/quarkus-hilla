94e448d25fa8f0fe10a618f8561b398954ea8515
Dario Götze <dario@ok.de>
2023-04-23 13:41:57 +0200

    refactor: extract super class DropStatementMethodNode from DropInitStatementMethodNode

--- hilla-test-extension/deployment/src/main/java/org/acme/hilla/test/extension/deployment/asm/DropInitStatementMethodNode.java
+++ hilla-test-extension/deployment/src/main/java/org/acme/hilla/test/extension/deployment/asm/DropInitStatementMethodNode.java
@@ -10,57 +10,14 @@ import org.objectweb.asm.tree.TypeInsnNode;
 import java.util.ListIterator;
 import java.util.Set;
 
-public class DropInitStatementMethodNode extends MethodNode {
-
-    private final Set<String> classNames;
+public class DropInitStatementMethodNode extends DropStatementMethodNode {
 
     public DropInitStatementMethodNode(int access, String name, String desc,
                                        String signature, String[] exceptions, MethodVisitor mv, Set<String> classNames) {
-        super(Gizmo.ASM_API_VERSION, access, name, desc, signature, exceptions);
-        this.classNames = classNames;
-        this.mv = mv;
+        super(Gizmo.ASM_API_VERSION, access, name, desc, signature, exceptions, mv, (node) -> isTypeInsn(node, classNames));
     }
 
-    @Override
-    public void visitEnd() {
-        var iter = instructions.iterator();
-        while (iter.hasNext()) {
-            var typeInsnNode = findTypeInsn(iter);
-            if (typeInsnNode != null) {
-                removeUntilPreviousLabel(iter);
-                removeUntilNextLabel(iter);
-            }
-        }
-        accept(mv);
-    }
-
-    private TypeInsnNode findTypeInsn(ListIterator<AbstractInsnNode> iter) {
-        while (iter.hasNext()) {
-            var instruction = iter.next();
-            if (instruction instanceof TypeInsnNode && classNames.contains(((TypeInsnNode) instruction).desc)) {
-                return (TypeInsnNode) instruction;
-            }
-        }
-        return null;
-    }
-
-    private void removeUntilPreviousLabel(ListIterator<AbstractInsnNode> iter) {
-        while (iter.hasPrevious()) {
-            var instruction = iter.previous();
-            iter.remove();
-            if (instruction instanceof LabelNode) {
-                break;
-            }
-        }
-    }
-
-    private void removeUntilNextLabel(ListIterator<AbstractInsnNode> iter) {
-        while (iter.hasNext()) {
-            var instruction = iter.next();
-            if (instruction instanceof LabelNode) {
-                break;
-            }
-            iter.remove();
-        }
+    private static boolean isTypeInsn(AbstractInsnNode instruction, Set<String> classNames) {
+        return instruction instanceof TypeInsnNode && classNames.contains(((TypeInsnNode) instruction).desc);
     }
 }
