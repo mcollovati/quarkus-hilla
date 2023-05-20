68c5c27a5b1257a244d6d0608bd712a48b113da1
Marco Collovati <mcollovati@gmail.com>
2023-05-20 16:45:29 +0200

    add test to check endpoints are created at startup in dev mode

--- integration-tests/smoke-tests/src/test/java/com/example/application/BootstrapTest.java
+++ integration-tests/smoke-tests/src/test/java/com/example/application/BootstrapTest.java
@@ -1,4 +1,40 @@
 package com.example.application;
 
-public class BootstrapTest {
+import java.nio.file.Path;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
+
+import com.jayway.awaitility.Awaitility;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@QuarkusTest
+@QuarkusTestResource(HillaCleaner.class)
+class BootstrapTest {
+
+    @HillaFrontendGenerated
+    Path frontendGeneratedFolder;
+
+    @Test
+    void devMode_applicationStart_endpointsGenerated() {
+        assertThat(frontendGeneratedFolder).isDirectory().exists();
+        Awaitility.await().atMost(15, TimeUnit.SECONDS)
+                .until(() -> assertThat(frontendGeneratedFolder)
+                        .isDirectoryContaining(fileWithName("endpoints.ts"))
+                        .isDirectoryContaining(fileWithName("vaadin.ts"))
+                        .isDirectoryContaining(fileWithName("theme.js"))
+                        .isDirectoryContaining(
+                                fileWithName("UserInfoEndpoint.ts"))
+                        .isDirectoryContaining(
+                                fileWithName("HelloWorldEndpoint.ts"))
+                        .isDirectoryRecursivelyContaining(
+                                "glob:**/entities/User*.ts"));
+    }
+
+    private static Predicate<Path> fileWithName(String expectedFileName) {
+        return path -> path.getFileName().toString().equals(expectedFileName);
+    }
 }
