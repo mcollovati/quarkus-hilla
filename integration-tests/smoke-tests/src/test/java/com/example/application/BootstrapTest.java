2d08cdce42032299521e1a4fafbd10c94166dd79
Marco Collovati <mcollovati@gmail.com>
2023-08-21 11:11:16 +0200

    test: wait for dev-server to complete startup before starting tests (#203)
    Also increase bootstrap test timeout to wait for npm packages downloads
    and fixes selenide reports storage configuration.

--- integration-tests/smoke-tests/src/test/java/com/example/application/BootstrapTest.java
+++ integration-tests/smoke-tests/src/test/java/com/example/application/BootstrapTest.java
@@ -39,7 +39,7 @@ class BootstrapTest {
     @Test
     void devMode_applicationStart_endpointsGenerated() {
         assertThat(frontendGeneratedFolder).isDirectory().exists();
-        Awaitility.await().atMost(120, TimeUnit.SECONDS).untilAsserted(() -> assertThat(frontendGeneratedFolder)
+        Awaitility.await().atMost(5, TimeUnit.MINUTES).untilAsserted(() -> assertThat(frontendGeneratedFolder)
                 .isDirectoryContaining(fileWithName("endpoints.ts"))
                 .isDirectoryContaining(fileWithName("vaadin.ts"))
                 .isDirectoryContaining(fileWithName("theme.js"))
