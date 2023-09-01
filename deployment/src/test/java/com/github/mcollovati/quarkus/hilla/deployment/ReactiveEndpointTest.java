9ec769b8574ae8ca93df80f701a9baf4d6942b55
Marco Collovati <mcollovati@gmail.com>
2023-09-01 18:17:01 +0200

    increase poll time

--- deployment/src/test/java/com/github/mcollovati/quarkus/hilla/deployment/ReactiveEndpointTest.java
+++ deployment/src/test/java/com/github/mcollovati/quarkus/hilla/deployment/ReactiveEndpointTest.java
@@ -145,7 +145,7 @@ class ReactiveEndpointTest {
 
     private static void assertThatPushUpdateHasBeenReceived(HillaPushClient client, int i) throws InterruptedException {
         LOGGER.info("================== Asserting message {} for client {}", i, client.id);
-        client.assertMessageReceived(1, TimeUnit.SECONDS, message -> message.as("Message %d", i)
+        client.assertMessageReceived(5, TimeUnit.SECONDS, message -> message.as("Message %d", i)
                 .isEqualTo("{\"@type\":\"update\",\"id\":\"%s\",\"item\":%s}", client.id, i));
     }
 
