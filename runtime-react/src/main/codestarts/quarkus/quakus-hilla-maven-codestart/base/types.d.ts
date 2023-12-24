35ee4a667361a0a935e265989c0bf9b7252af517
Marco Collovati <mcollovati@gmail.com>
2023-12-24 14:42:19 +0100

    chore: update codestart (#503)

--- runtime-react/src/main/codestarts/quarkus/quakus-hilla-maven-codestart/base/types.d.ts
+++ runtime-react/src/main/codestarts/quarkus/quakus-hilla-maven-codestart/base/types.d.ts
@@ -1,22 +1,22 @@
 declare module '*.module.css' {
-  declare const styles: Record<string, string>;
-  export default styles;
+    declare const styles: Record<string, string>;
+    export default styles;
 }
 declare module '*.module.sass' {
-  declare const styles: Record<string, string>;
-  export default styles;
+    declare const styles: Record<string, string>;
+    export default styles;
 }
 declare module '*.module.scss' {
-  declare const styles: Record<string, string>;
-  export default styles;
+    declare const styles: Record<string, string>;
+    export default styles;
 }
 declare module '*.module.less' {
-  declare const classes: Record<string, string>;
-  export default classes;
+    declare const classes: Record<string, string>;
+    export default classes;
 }
 declare module '*.module.styl' {
-  declare const classes: Record<string, string>;
-  export default classes;
+    declare const classes: Record<string, string>;
+    export default classes;
 }
 
 /* CSS FILES */
@@ -28,34 +28,34 @@ declare module '*.styl';
 
 /* IMAGES */
 declare module '*.svg' {
-  const ref: string;
-  export default ref;
+    const ref: string;
+    export default ref;
 }
 declare module '*.bmp' {
-  const ref: string;
-  export default ref;
+    const ref: string;
+    export default ref;
 }
 declare module '*.gif' {
-  const ref: string;
-  export default ref;
+    const ref: string;
+    export default ref;
 }
 declare module '*.jpg' {
-  const ref: string;
-  export default ref;
+    const ref: string;
+    export default ref;
 }
 declare module '*.jpeg' {
-  const ref: string;
-  export default ref;
+    const ref: string;
+    export default ref;
 }
 declare module '*.png' {
-  const ref: string;
-  export default ref;
+    const ref: string;
+    export default ref;
 }
 declare module '*.avif' {
-  const ref: string;
-  export default ref;
+    const ref: string;
+    export default ref;
 }
 declare module '*.webp' {
-  const ref: string;
-  export default ref;
+    const ref: string;
+    export default ref;
 }
